a4a3a0f0775fcf9d8f154c985ba12684dd19c594
aLANparty <aLANparty@protonmail.com>
2024-06-24 18:28:45 -0400

    hardhat structure updates

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,30 +1,18 @@
 import * as dotenv from "dotenv";
 
-import { HardhatUserConfig, task } from "hardhat/config";
-import "@nomiclabs/hardhat-etherscan";
-import "@nomiclabs/hardhat-waffle";
-import "hardhat-gas-reporter";
-import "solidity-coverage";
-import "hardhat-abi-exporter";
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+// import "@nomiclabs/hardhat-etherscan";
+// import "@nomiclabs/hardhat-waffle";
+// import "hardhat-gas-reporter";
+// import "solidity-coverage";
+// import "hardhat-abi-exporter";
 
 dotenv.config();
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
 const config: HardhatUserConfig = {
   solidity: {
-    version: "0.8.20",
+    version: "0.8.24",
     settings: {
       optimizer: {
         enabled: true,
