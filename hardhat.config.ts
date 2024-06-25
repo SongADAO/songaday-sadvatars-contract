766bb2c4f26ae376b3c89cf10f7b7ae664618546
aLANparty <aLANparty@protonmail.com>
2024-06-25 11:58:06 -0400

    deploy script

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,60 +2,46 @@ import * as dotenv from "dotenv";
 
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-// import "@nomiclabs/hardhat-etherscan";
-// import "@nomiclabs/hardhat-waffle";
-// import "hardhat-gas-reporter";
-// import "solidity-coverage";
-// import "hardhat-abi-exporter";
+import "hardhat-gas-reporter";
+import "hardhat-abi-exporter";
 
 dotenv.config();
 
 const config: HardhatUserConfig = {
+  etherscan: {
+    apiKey: String(process.env.ETHERSCAN_API_KEY),
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+    coinmarketcap: String(process.env.COINMARKETCAP_API_KEY),
+    gasPrice: 60,
+  },
+  networks: {
+    localhost: {
+      from: "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266", // want this contract owner
+      url: "http://127.0.0.1:8545",
+    },
+    mainnet: {
+      accounts: [String(process.env.MAINNET_PRIVATE_KEY)],
+      from: process.env.MAINNET_FROM, // want this contract owner
+      url: process.env.MAINNET_URL,
+    },
+    sepolia: {
+      accounts: [String(process.env.SEPOLIA_PRIVATE_KEY)],
+      from: process.env.SEPOLIA_FROM, // want this contract owner
+      url: process.env.SEPOLIA_URL,
+    },
+  },
   solidity: {
-    version: "0.8.24",
     settings: {
       optimizer: {
         enabled: true,
         runs: 200,
       },
     },
+    version: "0.8.24",
   },
-  networks: {
-    goerli: {
-      url: process.env.GOERLI_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    },
-    rinkeby: {
-      url: process.env.RINKEBY_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    },
-    ropsten: {
-      url: process.env.ROPSTEN_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    },
-  },
-  gasReporter: {
-    enabled: process.env.REPORT_GAS !== undefined,
-    currency: "USD",
-    coinmarketcap: process.env.COINMARKETCAP_API_KEY || "",
-    gasPrice: 60,
-  },
-  etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
-  },
-  abiExporter: [
-    {
-      path: "./abi/pretty",
-      pretty: true,
-    },
-    {
-      path: "./abi/ugly",
-      pretty: false,
-    },
-  ],
 };
 
 export default config;
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,46 +1,19 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
 import { ethers } from "hardhat";
 
 async function main() {
-  function strToByte32(str: string) {
-    return "0x" + Buffer.from(str).toString("hex").padEnd(64, "0");
-  }
+  const tokenFactory = await ethers.getContractFactory("RRALTTP");
 
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
-
-  // We get the contract to deploy
-  const contract = await ethers.getContractFactory("SongADayPFP");
-  const token = await contract.deploy(
+  const token = await tokenFactory.deploy(
     process.env.TOKEN_BASE_URI || "",
     process.env.TOKEN_BASE_URI_PREFIX || "",
-    process.env.MINTER_ADDRESS || "",
-    process.env.MINTER_ADDRESS || "",
+    process.env.BENEFICIARY_ADDRESS || "",
+    process.env.ADMIN_ADDRESS || "",
+    process.env.ADMIN_ADDRESS || "",
     process.env.MINTER_ADDRESS || "",
   );
+  await token.waitForDeployment();
 
-  await token.deployed();
-
-  // await token.setMaxPerWallet(1);
-
-  // const minterAddress = process.env.MINTER_ADDRESS || "";
-  // if (minterAddress) {
-  //   const minterRoleParam = ethers.utils.keccak256(
-  //     ethers.utils.toUtf8Bytes("MINTER_ROLE")
-  //   );
-
-  //   await token.grantRole(minterRoleParam, minterAddress);
-  // }
-
-  console.log("Contract deployed to:", token.address);
+  console.log(`Wrapper contract deployed to ${await token.getAddress()}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
