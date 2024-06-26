097b21f2239003b5e4eb2d322800bf9ec9d9aecd
aLANparty <aLANparty@protonmail.com>
2024-06-26 16:49:27 -0400

    add symbol

--- contracts/SADvatars.sol
+++ contracts/SADvatars.sol
@@ -83,7 +83,7 @@ contract SADvatars is
         }
 
         // solhint-disable-next-line quotes
-        string memory json = '{"name": "SADvatars", "description": "Song a Day Avatars", "image": "https://songaday.world/sadvatars/image.png", "banner_image": "https://songaday.world/sadvatars/banner-image.png", "featured_image": "https://songaday.world/sadvatars/featured-image.png", "external_link": "https://songaday.world/sadvatars", "collaborators": ["0x2a2C412c440Dfb0E7cae46EFF581e3E26aFd1Cd0"]}';
+        string memory json = '{"name": "SADvatars", "symbol": "SADVATARS", "description": "Song a Day Avatars", "image": "https://songaday.world/sadvatars/image.png", "banner_image": "https://songaday.world/sadvatars/banner-image.png", "featured_image": "https://songaday.world/sadvatars/featured-image.png", "external_link": "https://songaday.world/sadvatars", "collaborators": ["0x2a2C412c440Dfb0E7cae46EFF581e3E26aFd1Cd0"]}';
 
         return string.concat("data:application/json;utf8,", json);
     }
